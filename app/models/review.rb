378683effb48c0b23ae6662aa81de61e489492c9
William Lee <github@william-lee.com>
2013-03-10 19:26:35 +0000

    changes default scope in model to createt_at DESC

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,8 @@
 class Review < ActiveRecord::Base
   attr_accessible :content, :name, :source_title
 
+  default_scope order('created_at DESC')
+  
   validates :content, length: { minimum: 3, maximum: 100 }, presence: true
   validates :name, length: {minimum:3 , maximum: 30 }, presence: true
 end
