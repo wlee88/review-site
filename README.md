cc2f596c9b56f3fb8ca6948c420c66c80945e321
William Lee <github@william-lee.com>
2013-03-10 13:37:46 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Three presidents review site #
+A git repository for a small spotaneous idea to make a website for the reviews of the latest iPhase album - Three presidents. click [here](http://superiphase3ds.bandcamp.com) for a listen
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Threepresidents::Application.config.secret_token = 'd9153a039f9254296a465ef4cacf564b0dd543ae806ff6582dd6e0bd22e62258db500ae4932e7ea3d473b2870834210c9e82eda30fb94e7069177af0c7ff3baa'
