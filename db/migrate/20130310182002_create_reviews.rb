79988729ffd7041af70617e4066f40ab82b2aae2
William Lee <github@william-lee.com>
2013-03-10 19:43:26 +0000

    changes content type from string to text

--- db/migrate/20130310182002_create_reviews.rb
+++ db/migrate/20130310182002_create_reviews.rb
@@ -1,7 +1,7 @@
 class CreateReviews < ActiveRecord::Migration
   def change
     create_table :reviews do |t|
-      t.string :name
+      t.text :name
       t.text :content
       t.text :source_title
 
